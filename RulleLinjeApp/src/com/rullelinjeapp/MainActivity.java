b93569bc4c1af738372064d09d07ab09edd0d4d9
unknown <simenhg@simenhg-PC.(none)>
2012-07-08 21:54:05 +0200

    La Til Android prosjekt

---
+++ RulleLinjeApp/src/com/rullelinjeapp/MainActivity.java
@@ -0,0 +1,32 @@
+package com.rullelinjeapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.support.v4.app.NavUtils;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        final Button button = (Button) findViewById(R.id.instruksjon);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	 Toast.makeText(getApplicationContext(), "En liten toast", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
---
+++ RulleLinjeApp/src/com/rullelinjeapp/Result.java
@@ -0,0 +1,17 @@
+package com.rullelinjeapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class Result extends Activity {
+
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
+
+
+}
