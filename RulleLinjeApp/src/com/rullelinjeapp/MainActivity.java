8846c6c93b8ebffd1ca802f3e59cb552b439f8c2
Simen Haugerud Granlund <simen@hgranlund.com>
2012-07-09 20:59:13 +0200

    La til dialog til instuksjoner

--- RulleLinjeApp/src/com/rullelinjeapp/MainActivity.java
+++ RulleLinjeApp/src/com/rullelinjeapp/MainActivity.java
@@ -1,32 +1,63 @@
 package com.rullelinjeapp;
-
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.support.v4.app.NavUtils;
 
 public class MainActivity extends Activity {
 
+	final Context context = this;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setUpDialog();
         
-        final Button button = (Button) findViewById(R.id.instruksjon);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-            	 Toast.makeText(getApplicationContext(), "En liten toast", Toast.LENGTH_SHORT).show();
-            }
-        });
+        
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
+    }
+    
+    private void setUpDialog(){
+        final Dialog dialog = new Dialog(context);
+		dialog.setContentView(R.layout.dialog_instruksjoner);
+		dialog.setTitle("Instruksjoner");
+
+		TextView text = (TextView) dialog.findViewById(R.id.text);
+		text.setText(R.string.instruksjoner_hovedside);
+
+		Button dialogButton = (Button) dialog.findViewById(R.id.dialogButtonOK);
+		dialogButton.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				dialog.dismiss();
+			}
+		});
+        
+        final Button button = (Button) findViewById(R.id.instruksjon);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	dialog.show();
+            }
+        });
     }
 }
