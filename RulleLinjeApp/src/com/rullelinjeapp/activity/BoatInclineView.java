6ec3c58954bf94c5169e5cf4c90a459d45bcc2c2
George Lee <georgekw@stud.ntnu.no>
2012-07-21 18:43:06 +0200

    aaaa

--- RulleLinjeApp/src/com/rullelinjeapp/activity/BoatInclineView.java
+++ RulleLinjeApp/src/com/rullelinjeapp/activity/BoatInclineView.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package com.rullelinjeapp.activity;
 
 import android.app.Activity;
@@ -14,3 +15,21 @@ public class BoatInclineView extends Activity{
 		setContentView(R.layout.activity_result);
 	}
 }
+=======
+package com.rullelinjeapp.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.rullelinjeapp.R;
+
+public class BoatInclineView extends Activity{
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_result);
+	}
+}
+>>>>>>> 5441153a9da9b9ca0c434b283300d85603bfe13d
