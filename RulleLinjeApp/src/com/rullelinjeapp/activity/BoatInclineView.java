bf145208b94e0427cda700ae95017f91ee903785
Simen Haugerud Granlund <simen@hgranlund.com>
2012-07-21 18:05:56 +0200

    fix

---
+++ RulleLinjeApp/src/com/rullelinjeapp/activity/BoatInclineView.java
@@ -0,0 +1,16 @@
+package com.rullelinjeapp.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.rullelinjeapp.R;
+
+public class BoatInclineView extends Activity{
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_result);
+	}
+}
