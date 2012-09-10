357f4ad32143cdffa4541be1790b03af2125c48e
Teodor Ande Elstad <teodoran@stud.ntnu.no>
2012-09-10 18:16:09 +0200

    Added functionality for sending email with results attached

--- RulleLinjeApp/src/com/rullelinjeapp/activity/CameraActivity.java
+++ RulleLinjeApp/src/com/rullelinjeapp/activity/CameraActivity.java
@@ -13,7 +13,6 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 import com.rullelinjeapp.R;
 
